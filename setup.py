e85e9dca54afd0cdd864af19f739db18eaafed20
Usama Muhammadi <musamazia736@gmail.com>
2022-11-30 11:34:03 +0000

    base commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Part of project to build mobile application scan and detect pages being flipped or not',
+    author='Usama Zia',
+    license='MIT',
+)
